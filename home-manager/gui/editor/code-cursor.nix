9e6ecbd3f6eb98f6435630fa8bd3ab6f69c2e7e3
purplesmoke05 <yosuke.otosu@gmail.com>
2025-09-14 00:03:28 +0900

    ♻️ refactor(code-cursor): update file copy commands and keybindings format
    - `code-cursor.nix`で、設定ファイルのコピー時に`--remove-destination`オプションを追加し、既存のファイルを上書きするように変更しました。
    - `cursor-keybindings.json`と`cursor-settings.json`で、キーとコマンドの順序を整理し、可読性を向上させました。
    - 不要な行を削除し、全体の構造を整えました。

--- home-manager/gui/editor/code-cursor.nix
+++ home-manager/gui/editor/code-cursor.nix
@@ -19,8 +19,8 @@ in
   home.activation = {
     writeCursorConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
       $DRY_RUN_CMD mkdir -p ${config.home.homeDirectory}/.config/Cursor/User
-      $DRY_RUN_CMD cp ${beautifyJson (builtins.toJSON (import ./vscode/settings.nix))} ${config.home.homeDirectory}/.config/Cursor/User/settings.json
-      $DRY_RUN_CMD cp ${beautifyJson (builtins.toJSON (import ./vscode/keybindings.nix))} ${config.home.homeDirectory}/.config/Cursor/User/keybindings.json
+      $DRY_RUN_CMD cp --remove-destination ${beautifyJson (builtins.toJSON (import ./vscode/settings.nix))} ${config.home.homeDirectory}/.config/Cursor/User/settings.json
+      $DRY_RUN_CMD cp --remove-destination ${beautifyJson (builtins.toJSON (import ./vscode/keybindings.nix))} ${config.home.homeDirectory}/.config/Cursor/User/keybindings.json
       $DRY_RUN_CMD chmod 644 ${config.home.homeDirectory}/.config/Cursor/User/settings.json
       $DRY_RUN_CMD chmod 644 ${config.home.homeDirectory}/.config/Cursor/User/keybindings.json
     '';
