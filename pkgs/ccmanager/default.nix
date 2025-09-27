d58ae869e5530265540ded74b6da1e5667dd7ab1
purplesmoke05 <15183665+purplesmoke05@users.noreply.github.com>
2025-09-27 00:29:46 +0000

    chore(ccmanager): Update to latest version

--- pkgs/ccmanager/default.nix
+++ pkgs/ccmanager/default.nix
@@ -6,16 +6,16 @@
 
 buildNpmPackage rec {
   pname = "ccmanager";
-  version = "2.4.0";
+  version = "2.6.1";
 
   src = fetchFromGitHub {
     owner = "kbwo";
     repo = "ccmanager";
-    rev = "v2.4.0";
-    hash = "sha256-lc0ObGpS3HfystNxpmC+BwNB+w4+8aWREHVoJRQVYQo=";
+    rev = "v2.6.1";
+    hash = "sha256-uDFvUzkGqVA6vHsANwONHUaE8K9K7Vy3yscxjxaywcU=";
   };
 
-  npmDepsHash = "sha256-iEkwFsffZxlqpHCOSukK56HuDN0dB3ZP4TVN1ectRac=";
+  npmDepsHash = "sha256-zK460FAwyPuu991dznhz5mjxS4FS0ArrVmRNgzlK4gA=";
 
   # Build configuration
   npmBuildScript = "build";
