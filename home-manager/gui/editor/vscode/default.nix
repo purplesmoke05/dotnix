8986e13bfa51c8d762ad44f1c4e10d607173e703
Yosuke Otosu <y-otosu@boostry.co.jp>
2026-03-28 00:39:00 +0900

    ♻️ refactor(vscode): drop custom package management

--- home-manager/gui/editor/vscode/default.nix
+++ home-manager/gui/editor/vscode/default.nix
@@ -32,59 +32,6 @@ in
     profiles.default = {
       # Avoid HM-managed symlinked settings; activation writes real files. / HM 管理のシンボリックリンクを避け、activation で実ファイルを書き込む。
       keybindings = lib.mkForce [ ];
-      extensions = with pkgs.vscode-extensions; [
-        bbenoist.nix
-        tuttieee.emacs-mcx
-        aaron-bond.better-comments
-        streetsidesoftware.code-spell-checker
-        ms-vscode-remote.remote-containers
-        ms-azuretools.vscode-docker
-        mikestead.dotenv
-        hediet.vscode-drawio
-        tamasfe.even-better-toml
-        skyapps.fish-vscode
-        github.copilot
-        github.copilot-chat
-        enkia.tokyo-night
-        golang.go
-        oderwat.indent-rainbow
-        ms-ceintl.vscode-language-pack-ja
-        pkief.material-icon-theme
-        jnoortheen.nix-ide
-        christian-kohler.path-intellisense
-        # ms-python.python # Moved to marketplace extensions due to hash mismatch
-        ms-python.vscode-pylance
-        ms-python.debugpy
-        mechatroner.rainbow-csv
-        shardulm94.trailing-spaces
-        redhat.vscode-yaml
-        catppuccin.catppuccin-vsc
-      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-        {
-          name = "cform";
-          publisher = "aws-scripting-guy";
-          version = "0.0.24";
-          sha256 = "X3Om8uB94Va/uABnZhzm2ATbqj3wzqt/s2Z844lZcmU=";
-        }
-        {
-          name = "jumpy2";
-          publisher = "davidlgoldberg";
-          version = "1.7.0";
-          sha256 = "1j668ias08jkkz3v1fnljxphhkpgy0imbii2s9i0db390c07j1qf";
-        }
-        {
-          name = "zenkaku";
-          publisher = "mosapride";
-          version = "0.0.3";
-          sha256 = "0abbgg0mjgfy5495ah4iiqf2jck9wjbflvbfwhwll23g0wdazlr5";
-        }
-        /*{
-          name = "python";
-          publisher = "ms-python";
-          version = "2025.1.2025012401";
-          sha256 = "sha256-uD6NWGD5GyYwd7SeoGsgYEH26NI+hDxCx3f2EhqoOXk=";
-        }*/
-      ];
       # Only set userSettings for Linux
       userSettings = lib.mkForce { };
     };
