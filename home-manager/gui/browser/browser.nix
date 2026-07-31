c6484cf9fb2ab24c67cac3ab758496840e24aaab
purplesmoke05 <yosuke.otosu@gmail.com>
2026-07-31 13:24:13 +0900

    🧹 chore(brave): remove custom launch flags
    Brave に個別指定していた Wayland、VA-API、GPU 関連の起動引数を削除し、パッケージ側の既定設定に委ねる。Google Chrome の明示的な起動引数は維持する。
    
    あわせて効果のない brave-flags.conf のコメントアウト例を削除した。

--- home-manager/gui/browser/browser.nix
+++ home-manager/gui/browser/browser.nix
@@ -12,19 +12,5 @@
       "--enable-zero-copy"
     ];
     brave.enable = true;
-    # Use Home Manager's intended option for command line args
-    brave.commandLineArgs = [
-      "--enable-features=UseOzonePlatform,VaapiVideoDecodeLinuxGL"
-      "--ozone-platform=wayland"
-      "--enable-wayland-ime=true"
-      "--ignore-gpu-blocklist"
-      "--enable-gpu-rasterization"
-      "--enable-zero-copy"
-    ];
   };
-
-  # Removed ineffective brave-flags.conf definition
-  # xdg.configFile."brave-flags.conf".text = ''
-  #  ...
-  # '';
 }
